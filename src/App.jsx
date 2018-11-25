d11f13e600526a6e40adccdd309435bab96b6a9b
leo10099 <hancleandro@gmail.com>
2018-11-25 11:50:40 -0300

    prettier and eslint config ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import CssBaseline from "@material-ui/core/CssBaseline";
+import React, { Component } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 class App extends Component {
   render() {
---
+++ src/components/theme.js
@@ -0,0 +1,15 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const violet = '#3784FF';
+const cream = '#E8F0FF';
+const red = '#E84855';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: violet },
+    secondary: { main: cream },
+    error: { main: red }
+  }
+});
+
+export default theme;
