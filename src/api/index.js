70ce396bc3f4cc04ca51003b362ff4e94bf54058
leo10099 <hancleandro@gmail.com>
2018-11-25 21:28:58 -0300

    querystring implemented

--- src/api/index.js
+++ src/api/index.js
@@ -6,4 +6,10 @@ api.test = () => {
   return fetch(`${BASE_URL}/test`).then(response => response.json());
 };
 
+api.search = query => {
+  return fetch(`${BASE_URL}/search/?query=${query}`).then(response =>
+    response.json()
+  );
+};
+
 export default api;
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -3,6 +3,12 @@ import React, { Component } from 'react';
 import api from '../api/index';
 
 export default class Home extends Component {
+  componentDidMount() {
+    api
+      .search('java')
+      .then(response => console.log(response))
+      .catch(e => console.log(e));
+  }
   render() {
     return <section id="Home" />;
   }
