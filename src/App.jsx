fcc0bc25985d376e4e32afd820d00547406e94b7
leo10099 <hancleandro@gmail.com>
2018-12-02 11:33:09 -0300

    style links and add footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import theme from './components/theme';
 import Header from './components/Header';
-
+import Footer from './components/Footer';
 import './assets/scss/main.scss';
 
 class App extends Component {
@@ -12,6 +12,7 @@ class App extends Component {
       <div className="App">
         <MuiThemeProvider theme={theme}>
           <Header />
+          <Footer />
         </MuiThemeProvider>
       </div>
     );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import '../assets/scss/pages/Footer.scss';
+
 export default function Footer() {
   return (
-    <footer>
-      Hecho con ❤️ por{' '}
+    <footer className="footer">
+      Hecho con ❤️ por &nbsp;
       <a href="https://twitter.com/HancLeandro">Leandro Hanc</a>
     </footer>
   );
