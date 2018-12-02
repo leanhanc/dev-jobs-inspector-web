95a666d0b5a87d8becb85778113f76215f2b2f18
leo10099 <hancleandro@gmail.com>
2018-12-02 11:43:59 -0300

    override link styles in jobs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import '../assets/scss/pages/Footer.scss';
 export default function Footer() {
   return (
     <footer className="footer">
-      Hecho con ❤️ por &nbsp;
+      Hecho con&nbsp; ❤️ por &nbsp;
       <a href="https://twitter.com/HancLeandro">Leandro Hanc</a>
     </footer>
   );
