536c9dd7f2a2bacc422eaf87e544cdd1c26ee810
leo10099 <hancleandro@gmail.com>
2018-12-31 16:47:06 -0300

    add support for fallback api

--- src/api/index.js
+++ src/api/index.js
@@ -1,24 +1,35 @@
 const api = {};
 
 api.search = (query, locationFilter, dateFilter, page = 1) => {
-  return fetch(
-    `${process.env.REACT_APP_BASE_URL}/search/?query=${query}&locationFilter=${
-      locationFilter ? locationFilter : ''
-    }&dateFilter=${dateFilter ? dateFilter : ''}&page=${page}`
-  ).then(response => {
-    if (response.ok) {
-      return response.json();
-    } else {
+  return new Promise((resolve, reject) => {
+    fetch(
+      `${
+        process.env.REACT_APP_BASE_URL
+      }/search/?query=${query}&locationFilter=${
+        locationFilter ? locationFilter : ''
+      }&dateFilter=${dateFilter ? dateFilter : ''}&page=${page}`
+    ).catch(e => {
+      /* 
+      Se agrega soporte para un servidor como fallback. Se
+      lo llama si falla la conexión al servidor principal y si figura
+      como variable de entorno.
+      */
       if (process.env.REACT_APP_BASE_URL_FALLBACK) {
-        return fetch(
-          `${
-            process.env.REACT_APP_BASE_URL_FALLBACK
-          }/search/?query=${query}&locationFilter=${
-            locationFilter ? locationFilter : ''
-          }&dateFilter=${dateFilter ? dateFilter : ''}&page=${page}`
-        ).then(response => response.json());
+        if (e.message === 'Failed to fetch') {
+          return fetch(
+            `${
+              process.env.REACT_APP_BASE_URL_FALLBACK
+            }/search/?query=${query}&locationFilter=${
+              locationFilter ? locationFilter : ''
+            }&dateFilter=${dateFilter ? dateFilter : ''}&page=${page}`
+          )
+            .then(response => resolve(response.json()))
+            .catch(e => reject(e));
+        }
+      } else {
+        reject(e);
       }
-    }
+    });
   });
 };
 
