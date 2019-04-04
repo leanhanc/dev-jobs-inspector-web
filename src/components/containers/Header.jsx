e553bfd33fe8024fe1f35d64b56cd0fb4b1e4ecd
Leandro Hanc <leandrohanc@gmail.com>
2019-04-03 23:27:53 -0300

    Remove Material UI

--- src/components/containers/Header.jsx
+++ src/components/containers/Header.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import '../../styles/Header.css';
-import Autocomplete from '../presentational/Autocomplete';
+
 import logo from '../../assets/img/logo.png';
-import searchIcon from '../../assets/img/search-icon.svg';
 
 const Header = () => {
   return (
     <section id="Header">
       <img src={logo} alt="Dev Job Inspector Logo" className="Header__logo" />
-      <div>
-        <img src={searchIcon} className="Header__Autocomplete-Icon" />
-        <Autocomplete className={'Header__Autocomplete'} />
-      </div>
     </section>
   );
 };
