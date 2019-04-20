cd6a3cb6168df1fb0ccf50784a6848640b7ad3d5
Leandro Hanc <leandrohanc@gmail.com>
2019-04-20 20:33:54 -0300

    Fix unique key prop warning

--- src/components/containers/MainContent/Features/Features.jsx
+++ src/components/containers/MainContent/Features/Features.jsx
@@ -9,7 +9,7 @@ const Features = () => {
     <section id="About__Features">
       {featuresListContent.map(feature => {
         return (
-          <Card className="About__Features-Card">
+          <Card className="About__Features-Card" key={feature.title}>
             <div className="About__Features-CardContent">
               <Icon type={feature.icon} className="About__Features-CardIcon" />
               <h3 className="About__Features-CardTitle">{feature.title.toUpperCase()}</h3>
