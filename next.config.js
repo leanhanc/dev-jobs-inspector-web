7f195b17c6ce1b2387687c715c3da95286169355
leo10099 <leandrohanc@gmail.com>
2021-06-26 20:31:35 -0300

    Update dependencies

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 const path = require("path");
-const withImages = require("next-images");
 
 module.exports = {
-  ...withImages(),
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
     prependData: `
