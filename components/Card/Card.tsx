34f1d6db1bef965f6cca1c7e094dc4f756f43fd3
leo10099 <leandrohanc@gmail.com>
2022-03-17 15:54:12 -0300

    Style fixes and improvements across the board

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -5,20 +5,23 @@ import classNames from "classnames";
 import styles from "./Card.module.sass";
 
 interface CardProps {
-  className?: string;
-  Icon?: React.ReactNode;
-  title: string;
-  content: string;
+	className?: string;
+	Icon?: React.ReactNode;
+	title: string;
+	content: string;
 }
 
 const Card: React.FC<CardProps> = ({ Icon, title, className, content }) => {
-  return (
-    <div className={classNames(styles.Card, className)}>
-      {Icon && <div className={styles.CardIcon}>{Icon}</div>}
-      <h3 className={styles.CardTitle}>{title}</h3>
-      <p className={styles.CardContent}>{content}</p>
-    </div>
-  );
+	return (
+		<div className={classNames(styles.Card, className)}>
+			<div className={styles.CardHeader}>
+				{Icon && <div className={styles.CardIcon}>{Icon}</div>}
+				<h3 className={styles.CardTitle}>{title}</h3>
+			</div>
+
+			<p className={styles.CardContent}>{content}</p>
+		</div>
+	);
 };
 
 export default Card;
--- components/TypeaheadInput/TypeaheadInput.tsx
+++ components/TypeaheadInput/TypeaheadInput.tsx
@@ -11,85 +11,86 @@ import searchOptions from "data/searchOptions";
 import styles from "./TypeaheadInput.module.sass";
 
 interface TypeheadInputProps {
-  placeholder?: string;
-  Icon?: any;
-  searchTerm: string;
-  setSearchTerm?: React.Dispatch<React.SetStateAction<string>>;
+	placeholder?: string;
+	Icon?: any;
+	searchTerm: string;
+	setSearchTerm?: React.Dispatch<React.SetStateAction<string>>;
 }
 
 const TypeheadInputProps: React.FC<TypeheadInputProps> = ({
-  placeholder,
-  Icon,
-  searchTerm,
-  setSearchTerm,
+	placeholder,
+	Icon,
+	searchTerm,
+	setSearchTerm,
 }) => {
-  // State
-  const [isTypeaheadOpen, setIsTypeaheadOpen] = useState(false);
+	// State
+	const [isTypeaheadOpen, setIsTypeaheadOpen] = useState(false);
 
-  // Refs
-  const typeaheadContainerRef = useRef(null);
+	// Refs
+	const typeaheadContainerRef = useRef(null);
 
-  // Handlers
-  const onTypeaheadInsideClick = () => setIsTypeaheadOpen(true);
-  const onTypeaheadOutsideClick = () => setIsTypeaheadOpen(false);
-  const onInputTypeChange = (event: React.ChangeEvent<HTMLInputElement>) =>
-    setSearchTerm(event.target.value);
-  const onTypeaheadSelected = (event: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
-    setSearchTerm(event.currentTarget.id.split("-").pop());
-    setIsTypeaheadOpen(false);
-  };
+	// Handlers
+	const onTypeaheadInsideClick = () => setIsTypeaheadOpen(true);
+	const onTypeaheadOutsideClick = () => setIsTypeaheadOpen(false);
+	const onInputTypeChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+		setSearchTerm(event.target.value);
+	const onTypeaheadSelected = (event: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+		setSearchTerm(event.currentTarget.dataset.name);
+		setIsTypeaheadOpen(false);
+	};
 
-  // Memos
-  const filteredSearchOptions = useMemo(() => {
-    const regex = new RegExp(`^${searchTerm}`, `i`);
-    return searchOptions.sort().filter(option => regex.test(option));
-  }, [searchTerm]);
+	// Memos
+	const filteredSearchOptions = useMemo(() => {
+		const regex = new RegExp(`^${searchTerm}`, `i`);
+		return searchOptions.sort().filter(option => regex.test(option));
+	}, [searchTerm]);
 
-  // Hooks
-  useOnClickOutside(typeaheadContainerRef, onTypeaheadOutsideClick);
+	// Hooks
+	useOnClickOutside(typeaheadContainerRef, onTypeaheadOutsideClick);
 
-  return (
-    <div className="Input" ref={typeaheadContainerRef}>
-      <input
-        maxLength={32}
-        type="text"
-        className="BaseInput"
-        placeholder={placeholder}
-        value={searchTerm}
-        onChange={onInputTypeChange}
-        onClick={onTypeaheadInsideClick}
-      />
+	return (
+		<div className="Input" ref={typeaheadContainerRef}>
+			<input
+				maxLength={32}
+				type="text"
+				className="BaseInput"
+				placeholder={placeholder}
+				value={searchTerm}
+				onChange={onInputTypeChange}
+				onClick={onTypeaheadInsideClick}
+			/>
 
-      {Icon && (
-        <span className="InputIcon">
-          <Icon />
-        </span>
-      )}
+			{Icon && (
+				<span className="InputIcon">
+					<Icon />
+				</span>
+			)}
 
-      {searchTerm && (
-        <div
-          className={classnames(styles.TypeaheadInputContainer, {
-            [styles.TypeaheadInputContainerOpen]: isTypeaheadOpen,
-          })}
-        >
-          <ul className={styles.TypeaheadInputList}>
-            {filteredSearchOptions.map(searchOption => {
-              return (
-                <li
-                  className={styles.TypeaheadInputListItem}
-                  key={`option-${searchOption}`}
-                  onClick={onTypeaheadSelected}
-                  id={`option-${searchOption}`}
-                >
-                  {searchOption}
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      )}
-    </div>
-  );
+			{searchTerm && (
+				<div
+					className={classnames(styles.TypeaheadInputContainer, {
+						[styles.TypeaheadInputContainerOpen]: isTypeaheadOpen,
+					})}
+				>
+					<ul className={styles.TypeaheadInputList}>
+						{filteredSearchOptions.map(searchOption => {
+							return (
+								<li
+									className={styles.TypeaheadInputListItem}
+									key={`option-${searchOption}`}
+									onClick={onTypeaheadSelected}
+									id={`option-${searchOption}`}
+									data-name={searchOption}
+								>
+									{searchOption}
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default React.memo(TypeheadInputProps);
